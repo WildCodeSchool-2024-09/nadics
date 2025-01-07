00ced94dac6c10a852a8fc7e7b05140585d70d83
Cedric DAVASSOU <davassou.cedric.france@gmail.com>
2025-01-07 14:46:39 +0100

    ♻️  US17: PASSWORD_RECOVERY >  Create Password_Form

--- client/src/pages/PasswordRecovery.tsx
+++ client/src/pages/PasswordRecovery.tsx
@@ -1,20 +1,9 @@
-import { Link } from "react-router-dom";
+import Password_Form from "../components/Password_Form";
 
 export default function PasswordRecovery() {
   return (
     <div>
-      <h1>Forgot Password</h1>
-      <p>
-        Please enter the email address you'd like your password reset
-        information sent to
-      </p>
-      <form action="submit">
-        <input type="email" placeholder="email" name="email" id="email" />
-
-        <button type="button">request password reset</button>
-      </form>
-      <Link to="/login">Back to login</Link>
-      <p>Un mail vous a été envoyé pour réinitialiser votre mot de passe.</p>
+      <Password_Form />
     </div>
   );
 }
