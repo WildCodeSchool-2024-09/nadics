9e78d8fcc3b6fa1347e3d73dcbc46844af3343e1
ALEXANDRE JANASIEWICZ <alexandrejanasiewicz@gmail.com>
2025-01-14 14:53:55 +0100

    backend fini, rste à faire le front pour tester

--- server/src/router.ts
+++ server/src/router.ts
@@ -8,9 +8,11 @@ const router = express.Router();
 
 // Define item-related routes
 import userActions from "./users/userAction";
+import requestActions from "./request/requestActions";
 
 router.get("/api/users", userActions.browse);
 router.get("/api/users/:id", userActions.read);
+router.put("/api/request/:id", requestActions.edit);
 
 /* ************************************************************************* */
 
