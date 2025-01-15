ca75f8fc24b44e1c98d519e4c7fb26cd7a6d2a43
Nadir AMMISAID <nadir.ammisaid@outlook.com>
2025-01-15 12:01:18 +0100

    US25-Creation-de-Landing_Page-Maquette+Code-Mobile_Finished

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -11,6 +11,7 @@ import SignupPage from "./pages/SignupPage";
 
 // Import the main app component
 import App from "./App";
+import LandingPage from "./pages/LandingPage";
 import PasswordRecovery from "./pages/PasswordRecovery";
 import PostRequest from "./pages/PostRequest";
 
@@ -26,7 +27,11 @@ import PostRequest from "./pages/PostRequest";
 // You can add more routes as you build out your app!
 const router = createBrowserRouter([
   {
-    path: "/", // The root path
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/",
     element: <App />,
     children: [
       {
@@ -57,6 +62,47 @@ const router = createBrowserRouter([
   },
   // Try adding a new route! For example, "/about" with an About component
 ]);
+
+/* ************************************************************************* */
+// Original version for backup :
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/", // The root path
+//     element: <App />,
+//     children: [
+//       {
+//         path: "home",
+//         element: <HomePage />,
+//       },
+//       {
+//         path: "login",
+//         element: <LoginPage />,
+//       },
+//       {
+//         path: "signup",
+//         element: <SignupPage />,
+//       },
+//       {
+//         path: "password_recovery",
+//         element: <PasswordRecovery />,
+//       },
+//       {
+//         path: "display-user/:id",
+//         element: <DisplayUser />,
+//       },
+//       {
+//         path: "post_request",
+//         element: <PostRequest />,
+//       },
+//     ],
+//   },
+//   {
+//     path: "landingPage",
+//     element: <LandingPageComponent />,
+//   },
+//   // Try adding a new route! For example, "/about" with an About component
+// ]);
 
 /* ************************************************************************* */
 
